e25c14b3e59ab5b9946fdd4d32e9a46339db2555
Healler77 <raphaelgmail.com>
2020-08-13 14:07:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Weekthree from './week3.js';
+
+import './css/components.css';
+import './css/global.css';
+import './css/layout.css';
+import './css/sample-wallpaper.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weekthree />
+    </div>
+  );
+}
+
+export default App;
